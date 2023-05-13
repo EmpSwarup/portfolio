e4a1d3f53d4289bfa571fb102f3f2939a3c8d1c2
Swarup Sapkota <empswarupcoc@gmail.com>
2023-05-13 10:18:43 +0545

    added about section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./header.css";
+import Fade from "react-reveal/Fade";
 
 const Header = () => {
   /*=============== Toggle Menu ===============*/
@@ -7,53 +8,55 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">
-          Swarup
-        </a>
-
+        <Fade top>
+          <a href="index.html" className="nav__logo">
+            Swarup
+          </a>
+        </Fade>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
-              </a>
+              <Fade top delay={100}>
+                <a href="#home" className="nav__link active-link">
+                  <i className="uil uil-estate nav__icon"></i>Home
+                </a>
+              </Fade>
             </li>
-
-            <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
-                <i className="uil uil-estate nav__icon"></i>Home
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#about" className="nav__link">
-                <i className="uil uil-user nav__icon"></i>About
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon"></i>Skills
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#services" className="nav__link">
-                <i className="uil uil-briefcase nav__icon"></i>Services
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
-                <i className="uil uil-scenery nav__icon"></i>Portfolio
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#contact" className="nav__link">
-                <i className="uil uil-message nav__icon"></i>Contact
-              </a>
-            </li>
+            <Fade top delay={180}>
+              <li className="nav__item">
+                <a href="#about" className="nav__link">
+                  <i className="uil uil-user nav__icon"></i>About
+                </a>
+              </li>
+            </Fade>
+            <Fade top delay={300}>
+              <li className="nav__item">
+                <a href="#skills" className="nav__link">
+                  <i className="uil uil-file-alt nav__icon"></i>Skills
+                </a>
+              </li>
+            </Fade>
+            <Fade top delay={480}>
+              <li className="nav__item">
+                <a href="#services" className="nav__link">
+                  <i className="uil uil-briefcase nav__icon"></i>Services
+                </a>
+              </li>
+            </Fade>
+            <Fade top delay={590}>
+              <li className="nav__item">
+                <a href="#portfolio" className="nav__link">
+                  <i className="uil uil-scenery nav__icon"></i>Portfolio
+                </a>
+              </li>
+            </Fade>
+            <Fade top delay={680}>
+              <li className="nav__item">
+                <a href="#contact" className="nav__link">
+                  <i className="uil uil-message nav__icon"></i>Contact
+                </a>
+              </li>
+            </Fade>
           </ul>
           <i
             class="uil uil-times nav__close"
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+const Social = () => {
+  return (
+    <Fade left>
+      <div className="home__social">
+        <a
+          href="https://github.com/EmpSwarup"
+          className="home__social-icon"
+          target="_blank"
+        >
+          <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/swarup-sapkota-812031247/"
+          className="home__social-icon"
+          target="_blank"
+        >
+          <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a
+          href="https://www.behance.net/swarupsapkota"
+          className="home__social-icon"
+          target="_blank"
+        >
+          <i class="uil uil-behance-alt"></i>
+        </a>
+      </div>
+    </Fade>
+  );
+};
+
+export default Social;
