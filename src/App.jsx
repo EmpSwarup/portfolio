191274b54a3946652768d5750d54ad0ed9aa0182
Swarup Sapkota <empswarupcoc@gmail.com>
2023-05-13 14:27:12 +0545

    added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import PreLoader from "./components/preloader/PreLoader";
 
 function App() {
   return (
     <>
+      <PreLoader />
       <Header />
       <main className="main">
         <Home />
---
+++ src/components/preloader/PreLoader.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { preLoaderAnim } from "../../animations";
+import "./preloader.css";
+
+const PreLoader = () => {
+  useEffect(() => {
+    preLoaderAnim();
+  }, []);
+
+  return (
+    <div className="preloader">
+      <div className="texts-container">
+        <span>Student,</span>
+        <span> Developer,</span>
+        <span>Designer.</span>
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
